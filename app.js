88403a71197d2952a481e2fa8af0547a17a4fe7f
SUFIYAN <155263639+muhammadsufiyanattari@users.noreply.github.com>
2024-10-19 09:34:51 +0500

    update

--- app.js
+++ app.js
@@ -1,34 +1,25 @@
+let form = document.getElementById("form");
+let ul = document.getElementById("my-ul");
 
-let form = document.getElementById('form');
-let ul = document.getElementById('my-ul');
+let savedItems = localStorage.getItem("li");
+if (savedItems) {
+  // ul.innerHTML=localStorage.clear(savedItems)
+  ul.innerHTML = JSON.parse(savedItems);
+}
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // console.log(event);
 
-    let savedItems = localStorage.getItem('li');
-    if (savedItems) {
-        
-        ul.innerHTML = JSON.parse(savedItems);  
-    }
-   
+  let inputValue = event.target[0].value;
 
+  let li = document.createElement("li");
+  li.textContent = inputValue;
+  li.className = "todo-li";
 
+  ul.appendChild(li);
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-// console.log(event);
+  localStorage.setItem("li", JSON.stringify(ul.innerHTML));
 
-    let inputValue = event.target[0].value;
-
-   
-    let li = document.createElement('li');
-    li.textContent = inputValue;
-    li.className = 'todo-li';
-
-    ul.appendChild(li);
-    
-
-    localStorage.setItem('li', JSON.stringify(ul.innerHTML));
-  
-
-    event.target[0].value = '';
+  event.target[0].value = "";
 });
- 
